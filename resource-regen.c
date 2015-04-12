da71c37eca70456828f16272fc4ae1ef0463140e
Cluedrew <cluedrew@siductionbox>
2015-04-12 16:00:05 -0400

    Did more work on the warp-space modual and tranphered the basic data over for the resource-regen modual.

--- resource-regen.c
+++ resource-regen.c
@@ -1,11 +1,28 @@
 #include "resourse-regen.h"
 
+// Reimplementing the old resourse regeneration system.
+// (This is one that I do feel could use an upgrade.)
+#define NUM_RESOURCES 5
+Sprite resourceImages[NUM_RESOURCES];
+int resourceWeights[NUM_RESOURCES] = {20, 10, 20, 30, 20};
+
+#define ENUM_BIO 0 // BIO: Organic Material
+#define ENUM_DE  1 // DE: Dark Energy
+#define ENUM_EXM 2 // ExM: Exotic Matter
+#define ENUM_ORE 3 // Ore: aka Coal aka Coar
+#define ENUM_TEC 4 // Tech: Refined Goods
+
 
 
 // Loads items are prepares to start the game (more peramiters needed?)
 void load(void)
 {
-
+  // Load all sprites
+  resourceImages[ENUM_ORE] = loadSprite("Old/WDWDN/Ore.png");
+  resourceImages[ENUM_BIO] = loadSprite("Old/WDWDN/Bio.png");
+  resourceImages[ENUM_TEC] = loadSprite("Old/WDWDN/Tech.png");
+  resourceImages[ENUM_EXM] = loadSprite("Old/WDWDN/ExM.png");
+  resourceImages[ENUM_DE]  = loadSprite("Old/WDWDN/DE.png");
 }
 
 // Draw to the screen.
@@ -35,5 +52,9 @@ void onKeyUp(int key, const InputStateHANDLE is)
 // Clean up.
 void onQuit()
 {
-
+  // Free Resource Images.
+  for (int i = 0 ; i < NUM_RESOURCES ; ++i)
+  {
+    cleanSprite(resourceImages[i]);
+  }
 }
--- warp-space.c
+++ warp-space.c
@@ -1,19 +1,47 @@
 #include "warp-space.h"
 
-// portal_type portals[NUM_PORTALS] = ...
+#include "gui.h"
+
+
+
+// Portal Data
+#define NUM_PORTALS 6;
+
+typedef struct PortalTAB {
+  Sprite image; // Picture of the portal.
+  int x; // x coordinate of the portal's center.
+  int y; // y coordinate of the portal's center.
+} Portal, * PortalHANDLE;
+
+Portal portals[NUM_PORTALS];
+
+
+
 // int links[NUM_PORTALS][NUM_PORTALS] = ...
 /* Should we use a double array again? It cause some problems last time with
  * some portals being linked twice. With a bit of extra math we could store
  * everything once.
  */
-// ships_types [NUM_PLAYERS] = ...
+
+
+
+// Ship Data
+int numShips = -1;
+
+typedef struct ShipTAB {
+  Sprite image; // Picture of the ship;
+  int loc; // The location in warp-space, by portal number.
+} Ship, * ShipHANDLE
+
+ShipHANDLE ships = NULL;
 
 
 
 // Loads items are prepares to start the game (more peramiters needed?)
 void load(void)
 {
-
+  // Read in portal map?
+  // Get the number of players to get the number of ships.
 }
 
 // Draw to the screen.
